f5e001634a6974884f6cadab5f4b78d352dfac3d
everySunshine <1275016465@qq.com>
2022-03-07 12:45:25 +0800

    提交数据

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <i class="toutiao toutiao-dianzan2"></i>
+  </div>
+</template>
+<script>
+export default {
+  name: 'APP'
+}
+</script>
+
+<style lang="less">
+
+</style>
